00279156125c401c2248f9d2f7bdad63db87ac1f
Damian Dudycz <ddudycz@mobitouch.net>
2019-12-16 14:34:06 +0100

    Some work

--- Sources/HandyThings/HandyThings.swift
+++ Sources/HandyThings/HandyThings.swift
@@ -5,6 +5,12 @@
 //  Created by Home Dudycz on 09/12/2019.
 //
 
+import UIKit
+
 public func fatalError<ErrorType: RawRepresentable>(_ error: ErrorType) -> Never where ErrorType.RawValue == String {
     fatalError(error.rawValue)
+}
+
+public var isApplicationActive: Bool {
+    UIApplication.shared.applicationState == .active
 }
