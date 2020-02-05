aac35e016be19ad100c49c68fa4b3a547635292a
Damian Dudycz <ddudycz@mobitouch.net>
2020-02-05 20:55:00 +0100

    Oddzielony PhotoLibrary od AugmentedReality

---
+++ Sources/HandyThings/CGSize+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Home Dudycz on 05/02/2020.
+//
+
+import CoreGraphics
+
+public extension CGSize {
+    
+    init(square: CGFloat) {
+        self.init(width: square, height: square)
+    }
+    
+}
---
+++ Sources/HandyThings/IgnoreCourseMode.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Home Dudycz on 21/12/2019.
+//
+
+import CoreLocation
+
+public enum IgnoreCourseMode: CLLocationDirection {
+    case lookAtCamera = -1.0
+    case bilboard     = -2.0
+}
