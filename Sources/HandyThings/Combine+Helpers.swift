f5d07b03d85f19f63292fa67326c0eb22f74ea50
Damian Dudycz <ddudycz@mobitouch.net>
2020-03-27 21:27:52 +0100

    Working on reactive version

---
+++ Sources/HandyThings/Combine+Helpers.swift
@@ -0,0 +1,71 @@
+//
+//  File.swift
+//  
+//
+//  Created by Home Dudycz on 26/03/2020.
+//
+
+import Combine
+
+@propertyWrapper
+public class CurrentValue<Value> {
+    
+    public var wrappedValue: Value {
+        get { projectedValue.value }
+        set { projectedValue.value = newValue }
+    }
+    
+    public var projectedValue: CurrentValueSubject<Value, Never>
+    
+    public init(wrappedValue: Value) {
+        self.projectedValue = CurrentValueSubject(wrappedValue)
+    }
+    
+}
+
+public extension Publisher {
+    
+    typealias Change = (oldValue: Output, newValue: Output)
+    
+    func withPrevious(initialValue: Output) -> Publishers.Scan<Self, Change> {
+        let initialValue = initialValue
+        return scan((oldValue: initialValue, newValue: initialValue)) { (oldValue: $0.1, newValue: $1) }
+    }
+    
+}
+
+public extension CurrentValueSubject {
+        
+    func withPrevious(initialValue: Output? = nil) -> Publishers.Scan<CurrentValueSubject, Change> {
+        withPrevious(initialValue: initialValue ?? value)
+    }
+    
+}
+
+public extension Publisher where Failure == Never {
+    
+    func assignWeak<Root>(to keyPath: ReferenceWritableKeyPath<Root, Output>, on object: Root) -> AnyCancellable where Root: AnyObject {
+        sink { [weak object] (value) in
+            object?[keyPath: keyPath] = value
+        }
+    }
+
+    func assignUnowned<Root>(to keyPath: ReferenceWritableKeyPath<Root, Output>, on object: Root) -> AnyCancellable where Root: AnyObject {
+        sink { [unowned object] (value) in
+            object[keyPath: keyPath] = value
+        }
+    }
+
+}
+
+public extension AnyCancellable {
+    
+    func store(in cancellable: inout AnyCancellable?) {
+        cancellable = self
+    }
+    
+    func store<Key: Hashable>(in dictionary: inout [Key : AnyCancellable], for key: Key) {
+        dictionary[key] = self
+    }
+    
+}
