8e2c37b4b758cece0425445f1c82b1fa12263860
Damian Dudycz <ddudycz@mobitouch.net>
2020-06-13 14:20:33 +0200

    Lots of changes in WebService

---
+++ Sources/HandyThings/Data+AppendString.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by Home Dudycz on 12/06/2020.
+//
+
+import Foundation
+
+// TODO: Move to HandyThings.
+public extension Data {
+    
+    enum ConversionError: Error {
+        case failedToConvertToData
+    }
+    
+    mutating func append(_ string: String) throws {
+        guard let data = string.data(using: .utf8) else {
+            throw ConversionError.failedToConvertToData
+        }
+        append(data)
+    }
+    
+}
