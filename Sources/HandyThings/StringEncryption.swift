93e1e25701c4f79026be1b101c4ecbd5623537fd
Damian Dudycz <ddudycz@mobitouch.net>
2020-07-27 14:58:13 +0200

    Fixes in AES lib

--- Sources/HandyThings/StringEncryption.swift
+++ Sources/HandyThings/StringEncryption.swift
@@ -9,34 +9,52 @@ import Foundation
 
 public enum StringEncryption {
     
-    private static func cipher(for key: String, length: Int) -> [UInt8] {
+    public static func encrypt(string: String, using key: String) -> Data? {
+        guard let data = string.data(using: .utf8) else { return nil }
+        return DataEncryption.encrypt(data: data, using: key)
+    }
+    
+    public static func decrypt(data: Data, using key: String) -> String? {
+        let decryptedData = DataEncryption.decrypt(data: data, using: key)
+        return String(data: decryptedData, encoding: .utf8)
+    }
+        
+    public static func decrypt(file fileName: String, in bundle: Bundle, using key: String) -> String? {
+        guard let encryptedData = DataEncryption.decrypt(file: fileName, in: bundle, using: key) else { return nil }
+        return decrypt(data: encryptedData, using: key)
+    }
+    
+}
+
+public enum DataEncryption {
+    
+    static func cipher(for key: String, length: Int) -> [UInt8] {
         let keyChars = key.utf8
         let repeats = Int((Double(length) / Double(keyChars.count)).rounded(.up))
         return Array(((0..<repeats).flatMap { _ in keyChars })[0..<length])
     }
     
-    public static func encrypt(string: String, using key: String) -> Data {
-        let stringUTF8 = string.utf8
-        let cipher = self.cipher(for: key, length: stringUTF8.count)
-        let encryptedArray = stringUTF8.enumerated().map { (index, element) in
+    public static func encrypt(data: Data, using key: String) -> Data {
+        let cipher = DataEncryption.cipher(for: key, length: data.count)
+        let encryptedArray = data.enumerated().map { (index, element) in
             element ^ cipher[index]
         }
         return Data(encryptedArray)
     }
     
-    public static func decrypt(data: Data, using key: String) -> String? {
-        let cipher = self.cipher(for: key, length: data.count)
+    public static func decrypt(data: Data, using key: String) -> Data {
+        let cipher = DataEncryption.cipher(for: key, length: data.count)
         let decryptedArray = data.enumerated().map { (index, element) in
             element ^ cipher[index]
         }
-        return String(bytes: decryptedArray, encoding: .utf8)
+        return Data(decryptedArray)
     }
-        
-    public static func decrypt(file fileName: String, in bundle: Bundle, using key: String) -> String? {
+
+    public static func decrypt(file fileName: String, in bundle: Bundle, using key: String) -> Data? {
         guard let url = bundle.url(forResource: fileName, withExtension: nil), let data = try? Data(contentsOf: url) else {
             return nil
         }
         return decrypt(data: data, using: key)
     }
-    
+
 }
